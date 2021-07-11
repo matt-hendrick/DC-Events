5663104acf340429c585e7741e4f678866d91fd2
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-07-11 16:59:54 -0400

    Add: CSIS scraper

---
+++ scraper/csis_scraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from dateutil import parser
+
+
+def mei_scraper():
+
+    eventList = []
+
+    url = "https://www.csis.org/events-upcoming?title="
+
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for event in soup.find_all("div", class_="ds-right"):
+        title = event.find("div", class_="teaser__title").get_text()
+        dateTime = parser.parse(event.find(
+            "span", class_="date-display-single").get("content"))
+        dateTime = str(dateTime)
+        entity = "CSIS"
+        entityType = "Think Tank"
+        link = "https://www.csis.org" + event.find("a").get("href")
+        eventList.append({"entity": entity, "type": entityType,
+                         "dateTime": dateTime, "title": title, "link": link})
+
+    filename = "event_list.json"
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    for item in eventList:
+        data.append(item)
+
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+mei_scraper()
