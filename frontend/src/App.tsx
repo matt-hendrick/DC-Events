de1652ec8406b5867518bafc66be3bd31db151cb
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-08-01 12:05:57 -0400

    Update: Connected App.tsx to DynamoDB eventList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import eventList from './event_list.json';
+import { useState } from 'react';
+
+import axios from 'axios';
 
 import Container from '@material-ui/core/Container';
 
@@ -7,29 +9,46 @@ import Navbar from './components/Navbar/Navbar';
 import Card from './components/Card/Card';
 
 function App() {
-  eventList.sort(
-    (a, b) => new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime()
-  );
+  const [eventList, setEventList] = useState([] as any);
+
+  axios
+    .get(
+      'https://rlkww3p7g8.execute-api.us-east-2.amazonaws.com/default/DC_Events_API'
+    )
+    .then((response) => {
+      let tempEventList = response.data.body.Items;
+      setEventList(
+        tempEventList.sort(
+          (a: any, b: any) =>
+            new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime()
+        )
+      );
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 
   return (
     <div>
       <Navbar />
       <Container style={{ paddingTop: '5vh', width: '85%' }}>
-        {eventList.map((item: any) => {
-          const dateTime = new Date(item.dateTime);
-          return (
-            <Card
-              title={item.title}
-              dateTime={
-                dateTime.getHours() !== 0
-                  ? dateTime.toLocaleString()
-                  : dateTime.toLocaleDateString()
-              }
-              entity={item.entity}
-              link={item.link}
-            />
-          );
-        })}
+        {eventList
+          ? eventList.map((item: any) => {
+              const dateTime = new Date(item.dateTime);
+              return (
+                <Card
+                  title={item.title}
+                  dateTime={
+                    dateTime.getHours() !== 0
+                      ? dateTime.toLocaleString()
+                      : dateTime.toLocaleDateString()
+                  }
+                  entity={item.entity}
+                  link={item.link}
+                />
+              );
+            })
+          : null}
       </Container>
     </div>
   );
