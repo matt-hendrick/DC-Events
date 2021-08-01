3eb51faa453f202d9ac0b7ed8791fe73f9f5575e
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-08-01 11:00:34 -0400

    Update: connect app.py to DynamoDB table

--- scraper/app.py
+++ scraper/app.py
@@ -7,10 +7,33 @@ from csis_scraper import csis_scraper
 from heritage_scraper import heritage_scraper
 from middle_east_institute_scraper import middle_east_institute_scraper
 from rand_scraper import rand_scraper
+import boto3
 
-eventList = brookings_scraper() + atlantic_council_scraper() + cap_scraper() + \
-    csis_scraper() + heritage_scraper() + \
-    middle_east_institute_scraper() + rand_scraper()
-# carnegie_scraper()
 
-print(eventList)
+def app(dynamodb=None):
+    eventList = brookings_scraper() + atlantic_council_scraper() + cap_scraper() + \
+        csis_scraper() + heritage_scraper() + \
+        middle_east_institute_scraper() + rand_scraper()
+    # carnegie_scraper()
+    if not dynamodb:
+        dynamodb = boto3.resource(
+            'dynamodb', region_name='us-east-2')
+
+    table = dynamodb.Table('DC_Events')
+    for event in eventList:
+        # entity = event.entity
+        # entityType = event.entityType
+        # dateTime = event.dateTime
+        # title = event.ti
+        # {"entity": entity, "type": entityType,
+        #                  "dateTime": dateTime, "title": title, "link": link, "eventID": eventID, "unixTimeStamp": unixTimeStamp
+        # year = int(movie['year'])
+        # title = movie['title']
+        print("Adding event:", event)
+        table.put_item(Item=event)
+
+    # print(eventList)
+
+
+if __name__ == '__main__':
+    app()
