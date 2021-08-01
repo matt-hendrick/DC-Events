8d72d05d84e17e45ac0061fdba8e586bde39b4c1
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-08-01 13:22:14 -0400

    Add: Home page component. Event interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,17 @@
-import { useState, useEffect } from 'react';
-
-import axios from 'axios';
-
-// MUI
-import Container from '@material-ui/core/Container';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import { Fragment } from 'react';
 
 // Components
 import Navbar from './components/Navbar/Navbar';
-import Card from './components/Card/Card';
+
+// Pages
+import Home from './pages/Home/Home';
 
 function App() {
-  const [eventList, setEventList] = useState([] as any);
-
-  const getData = () => {
-    axios
-      .get(
-        'https://rlkww3p7g8.execute-api.us-east-2.amazonaws.com/default/DC_Events_API'
-      )
-      .then((response) => {
-        let tempEventList = response.data.body.Items;
-        setEventList(
-          tempEventList.sort(
-            (a: any, b: any) =>
-              new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime()
-          )
-        );
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
-    <div>
+    <Fragment>
       <Navbar />
-      <Container style={{ paddingTop: '5vh', width: '85%' }}>
-        {eventList?.length > 1 ? (
-          eventList.map((item: any) => {
-            const dateTime = new Date(item.dateTime);
-            return (
-              <Card
-                title={item.title}
-                dateTime={
-                  dateTime.getHours() !== 0
-                    ? dateTime.toLocaleString()
-                    : dateTime.toLocaleDateString()
-                }
-                entity={item.entity}
-                link={item.link}
-                key={item.uuid}
-              />
-            );
-          })
-        ) : (
-          <LinearProgress />
-        )}
-      </Container>
-    </div>
+      <Home />
+    </Fragment>
   );
 }
 
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+
+// Axios
+import axios from 'axios';
+
+// MUI
+import Container from '@material-ui/core/Container';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+// Components
+import Card from '../../components/Card/Card';
+
+interface Event {
+  entity: string;
+  entityType: string;
+  dateTime: string;
+  unixTimeStamp: number;
+  title: string;
+  link: string;
+  uuid: string;
+}
+
+function Home() {
+  const [eventList, setEventList] = useState<Event[]>([]);
+
+  const getData = () => {
+    axios
+      .get(
+        'https://rlkww3p7g8.execute-api.us-east-2.amazonaws.com/default/DC_Events_API'
+      )
+      .then((response) => {
+        let tempEventList = response.data.body.Items;
+        setEventList(
+          tempEventList.sort(
+            (a: Event, b: Event) =>
+              new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime()
+          )
+        );
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Container style={{ paddingTop: '5vh', width: '85%' }}>
+      {eventList?.length > 1 ? (
+        eventList.map((item: Event) => {
+          const dateTime = new Date(item.dateTime);
+          return (
+            <Card
+              title={item.title}
+              dateTime={
+                dateTime.getHours() !== 0
+                  ? dateTime.toLocaleString()
+                  : dateTime.toLocaleDateString()
+              }
+              entity={item.entity}
+              link={item.link}
+              key={item.uuid}
+            />
+          );
+        })
+      ) : (
+        <LinearProgress />
+      )}
+    </Container>
+  );
+}
+
+export default Home;
