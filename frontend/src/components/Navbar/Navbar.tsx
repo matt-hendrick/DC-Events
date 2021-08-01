d56d5ed0efe92136ec47c3fca7507414845d0ed9
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-08-01 13:22:36 -0400

    Minor cleanup: Navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,15 @@
+// MUI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 function Navbar() {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Events in DC</Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Events in DC</Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
