66f206f616198d6aa61e3f5e1237086466499d55
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-08-15 11:47:34 -0400

    Add: mui theme provider. Style: improve button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
 import { Fragment } from 'react';
 import './App.css';
 
+// Theme
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import themeObject from './utility/theme';
+
 // Components
 import Navbar from './components/Navbar/Navbar';
 
 // Pages
 import Home from './pages/Home/Home';
 
+const theme = createMuiTheme(themeObject);
+
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <div className="app-home">
-        <Home />
-      </div>
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Navbar />
+        <div className="app-home">
+          <Home />
+        </div>
+      </Fragment>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/components/MyButton/MyButton.tsx
+++ frontend/src/components/MyButton/MyButton.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 // MUI
 import Button from '@material-ui/core/Button';
+import { Theme, makeStyles } from '@material-ui/core';
 
 interface Props {
   children?: React.ReactNode | string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
+const useStyles = makeStyles<Theme, object>((theme) => ({
+  ...(theme.classes as object),
+}));
+
 function MyButton(props: Props) {
+  const classes = useStyles({} as object);
+
   return (
-    <Button onClick={props.onClick} color="primary" variant="contained">
+    <Button
+      onClick={props.onClick}
+      className={classes.button}
+      color="primary"
+      variant="contained"
+    >
       {props.children}
     </Button>
   );
