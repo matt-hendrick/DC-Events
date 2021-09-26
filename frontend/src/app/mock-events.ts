a71cc6ece688ec7138916ead1468d3e7fa871a85
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-09-26 12:12:26 -0400

    Add: home tests and mock-events file

---
+++ frontend/src/app/mock-events.ts
@@ -0,0 +1,33 @@
+export const MOCK_EVENTS = [
+  {
+    entity: 'Politics and Prose',
+    eventID: '2a447490-b13a-42fe-9c24-db6d926b27b6',
+    dateTime: '2021-09-27 19:00:00',
+    unixTimeStamp: 1632769200,
+    link: 'https://www.eventbrite.com/e/pp-live-jill-louise-busby-unfollow-me-with-jason-reynolds-tickets-165765006597',
+    additionalInfo: null,
+    type: 'Bookstore',
+    title: 'P&P Live! Jill Louise Busby — UNFOLLOW ME - with Jason Reynolds',
+  },
+  {
+    entity: 'Middle East Institute',
+    eventID: '5e84f256-0385-43ec-99c1-3a749af30443',
+    dateTime: '2021-11-02 10:00:00',
+    unixTimeStamp: 1635847200,
+    link: 'https://www.mei.edu/events/master-game-book-talk-martin-indyk',
+    additionalInfo: null,
+    type: 'Think Tank',
+    title: 'Master of the Game: A Book Talk with Martin Indyk',
+  },
+  {
+    entity: 'Politics and Prose',
+    eventID: '47b91cce-15ad-458e-acbf-af39babd124f',
+    dateTime: '2021-10-01 19:00:00',
+    unixTimeStamp: 1633114800,
+    link: 'https://www.eventbrite.com/e/pp-live-caoilinn-hughes-the-wild-laughter-with-mona-awad-tickets-169366572979',
+    additionalInfo: null,
+    type: 'Bookstore',
+    title:
+      "P&P Live! Caoilinn Hughes and Mona Awad | THE WILD LAUGHTER and ALL'S WELL",
+  },
+];
--- frontend/src/app/pages/home/home.component.spec.ts
+++ frontend/src/app/pages/home/home.component.spec.ts
@@ -1,25 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EventService } from 'src/app/services/event.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HomeComponent } from './home.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { MOCK_EVENTS } from '../../mock-events';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let compiled: HTMLElement;
+
+  let eventServiceStub: any;
 
   beforeEach(async () => {
+    eventServiceStub = {
+      getEvents(): Observable<any> {
+        return of(MOCK_EVENTS);
+      },
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      providers: [{ provide: EventService, useValue: eventServiceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render 3 event cards and displayedEvents should contain mock event data', () => {
+    expect(compiled.querySelectorAll('app-card').length).toEqual(3);
+    expect(component.displayedEvents.length).toEqual(3);
+    expect(component.displayedEvents[0].entity).toEqual(MOCK_EVENTS[0].entity);
+  });
+
+  it('should render 4 buttons', () => {
+    expect(compiled.querySelectorAll('button').length).toEqual(4);
   });
 });
