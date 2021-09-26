c79ecb5f5036897eecb8bc028d1cab59e50a1f92
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-09-26 13:17:23 -0400

    Test: add event service test

--- frontend/src/app/services/event.service.spec.ts
+++ frontend/src/app/services/event.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { EventService } from './event.service';
 
+import { MOCK_EVENTS } from '../mock-events';
+
 describe('EventsService', () => {
-  let service: EventService;
+  let eventService: EventService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EventService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    eventService = TestBed.inject(EventService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    controller.verify();
+  });
+
+  it('mocked GET call to the API returns mock data', (done) => {
+    eventService.getEvents().subscribe((response) => {
+      expect(response).toEqual(MOCK_EVENTS);
+      done();
+    });
+    const request = controller.expectOne(eventService.apiUrl);
+
+    request.flush({ body: { Items: MOCK_EVENTS } });
+
+    expect(request.request.method).toEqual('GET');
   });
 });
